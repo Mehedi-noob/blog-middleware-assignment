ad60a42ea2bcc6838be6ff59f73ca6c5e8111ca3
Mehedi-noob <mehedihasannoob@gmail.com>
2023-10-04 11:36:14 +0600

    feat: project done

--- index.js
+++ index.js
@@ -5,10 +5,10 @@ const _ = require('lodash');
 const app = express();
 const port = 3000;
 
-// Configure caching options
-const cacheOptions = { maxAge: 60000 }; // Cache results for 60 seconds (adjust as needed)
+// Custom cache object
+const customCache = new Map();
 
-// Middleware to fetch blog data from the third-party API
+// Define a function that fetches blog data
 const fetchBlogData = async () => {
   try {
     const response = await axios.get('https://intent-kit-16.hasura.app/api/rest/blogs', {
@@ -16,41 +16,47 @@ const fetchBlogData = async () => {
         'x-hasura-admin-secret': '32qR4KmXOIpsGPQKMqEJHGJS27G5s7HdSKO3gdtQd2kv5e852SiYwWNfxkZOBuQ6',
       },
     });
+    console.log(response.data.blogs);
 
-    return response.data; // Assuming the API returns an array of blog objects
+    return response.data.blogs; // Assuming the API returns an array of blog objects
   } catch (error) {
     console.error(error);
     throw new Error('Failed to fetch blog data');
   }
 };
 
-// Wrap the data retrieval function with memoization
-const memoizedFetchBlogData = _.memoize(fetchBlogData, cacheOptions);
+// Wrap the data retrieval function with memoization using the custom cache
+const memoizedFetchBlogData = _.memoize(fetchBlogData, (query) => customCache.has(query) ? customCache.get(query) : customCache.set(query, fetchBlogData(query)) && customCache.get(query));
 
-// Data Analysis Function
+// Define a function that analyzes blog data
 const analyzeBlogData = (blogs) => {
-  const totalBlogs = blogs.length;
-  const longestBlog = _.maxBy(blogs, 'title.length');
-  const blogsWithPrivacy = blogs.filter(blog => blog.title.toLowerCase().includes('privacy'));
-  const uniqueBlogTitles = _.uniqBy(blogs, 'title');
-
-  return {
-    totalBlogs,
-    longestBlog: longestBlog ? longestBlog.title : null,
-    blogsWithPrivacy: blogsWithPrivacy.length,
-    uniqueBlogTitles: uniqueBlogTitles.map(blog => blog.title),
+    if (!Array.isArray(blogs)) {
+      throw new Error('Invalid blog data. Expected an array.');
+    }
+  
+    const totalBlogs = blogs.length;
+    const longestBlog = _.maxBy(blogs, 'title.length');
+    const blogsWithPrivacy = blogs.filter(blog => blog.title.toLowerCase().includes('privacy'));
+    const uniqueBlogTitles = _.uniqBy(blogs, 'title');
+  
+    return {
+      totalBlogs,
+      longestBlog: longestBlog ? longestBlog.title : null,
+      blogsWithPrivacy: blogsWithPrivacy.length,
+      uniqueBlogTitles: uniqueBlogTitles.map(blog => blog.title),
+    };
   };
-};
+  
 
-// Wrap the data analysis function with memoization
-const memoizedAnalyzeBlogData = _.memoize(analyzeBlogData, cacheOptions);
+// Wrap the data analysis function with memoization using the custom cache
+const memoizedAnalyzeBlogData = _.memoize(analyzeBlogData, (blogs) => customCache.has(blogs) ? customCache.get(blogs) : customCache.set(blogs, analyzeBlogData(blogs)) && customCache.get(blogs));
 
 // Middleware to handle /api/blog-stats
 app.get('/api/blog-stats', async (req, res) => {
   try {
     // Fetch blog data using memoized function
     const blogs = await memoizedFetchBlogData();
-    
+
     // Analyze the data using memoized function
     const statistics = memoizedAnalyzeBlogData(blogs);
 
@@ -62,16 +68,29 @@ app.get('/api/blog-stats', async (req, res) => {
 });
 
 // Blog Search Endpoint
-app.get('/api/blog-search', (req, res) => {
-  const query = req.query.query.toLowerCase();
-
-  // Implement your search logic here
-
-  // Assuming you have an array of blogs from the previous middleware
-  const filteredBlogs = blogs.filter(blog => blog.title.toLowerCase().includes(query));
-
-  res.json(filteredBlogs);
-});
+app.get('/api/blog-search', async (req, res) => {
+    const query = req.query?.query?.toLowerCase();
+  
+    try {
+      // Fetch blog data using memoized function
+      const blogs = await memoizedFetchBlogData();
+  
+      // Analyze the data using memoized function
+      const statistics = memoizedAnalyzeBlogData(blogs);
+  
+      // Filter blogs based on the query
+      const filteredBlogs = blogs.filter(blog => blog.title.toLowerCase().includes(query));
+  
+      res.json(filteredBlogs);
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ error: 'Internal server error' });
+    }
+  });
+  
+  app.use('/', async(req, res)=>{
+    res.send("the server is running")
+  })
 
 // Error Handling Middleware
 app.use((err, req, res, next) => {
